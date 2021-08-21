84b600052f515aa0d5678472692d229f746adf13
coffmanjrp <coffmanjrp@gmail.com>
2021-08-21 00:13:29 -0700

    HTML and styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,60 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <h1>Drink Water</h1>
+      <h3>Goal: 2 Liters</h3>
+      <div className="cup">
+        <div className="remained">
+          <span>2L</span>
+          <small>Remained</small>
+        </div>
+        <div id="percentage" className="percentage"></div>
+      </div>
+      <p className="text">
+        Select how many glasses of water that you have drank
+      </p>
+      <div className="cups">
+        <div className="cup cup-small">
+          250
+          <br />
+          ml
+        </div>
+        <div className="cup cup-small">
+          250
+          <br />
+          ml
+        </div>
+        <div className="cup cup-small">
+          250
+          <br />
+          ml
+        </div>
+        <div className="cup cup-small">
+          250
+          <br />
+          ml
+        </div>
+        <div className="cup cup-small">
+          250
+          <br />
+          ml
+        </div>
+        <div className="cup cup-small">
+          250
+          <br />
+          ml
+        </div>
+        <div className="cup cup-small">
+          250
+          <br />
+          ml
+        </div>
+        <div className="cup cup-small">
+          250
+          <br />
+          ml
+        </div>
+      </div>
     </div>
   );
 }
