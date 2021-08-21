d74152b7780f9993c27de3e56295b27b107b766a
coffmanjrp <coffmanjrp@gmail.com>
2021-08-21 04:02:43 -0700

    Implement fill water event

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,59 @@
+import { useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [fullCups, setFullCups] = useState(0);
+  const cups = [1, 2, 3, 4, 5, 6, 7, 8];
+  const totalCups = cups.length;
+
   return (
     <div className="App">
       <h1>Drink Water</h1>
       <h3>Goal: 2 Liters</h3>
       <div className="cup">
-        <div className="remained">
+        <div
+          className="remained"
+          style={
+            fullCups === totalCups
+              ? { visibility: 'hidden', height: 0 }
+              : { visibility: 'visible' }
+          }
+        >
           <span>2L</span>
           <small>Remained</small>
         </div>
-        <div id="percentage" className="percentage"></div>
+        <div
+          className="percentage"
+          style={
+            fullCups === 0
+              ? { visibility: 'hidden', height: 0 }
+              : {
+                  visibility: 'visible',
+                  height: `${(fullCups / totalCups) * 330}px`,
+                }
+          }
+        >
+          {(fullCups / totalCups) * 100} %
+        </div>
       </div>
       <p className="text">
         Select how many glasses of water that you have drank
       </p>
       <div className="cups">
-        <div className="cup cup-small">
-          250
-          <br />
-          ml
-        </div>
-        <div className="cup cup-small">
-          250
-          <br />
-          ml
-        </div>
-        <div className="cup cup-small">
-          250
-          <br />
-          ml
-        </div>
-        <div className="cup cup-small">
-          250
-          <br />
-          ml
-        </div>
-        <div className="cup cup-small">
-          250
-          <br />
-          ml
-        </div>
-        <div className="cup cup-small">
-          250
-          <br />
-          ml
-        </div>
-        <div className="cup cup-small">
-          250
-          <br />
-          ml
-        </div>
-        <div className="cup cup-small">
-          250
-          <br />
-          ml
-        </div>
+        {cups.length > 0 &&
+          cups.map((cup) => (
+            <div
+              key={cup}
+              className={`cup cup-small${fullCups >= cup ? ' full' : ''}`}
+              onClick={() =>
+                setFullCups((prev) => (prev === cup ? prev - 1 : cup))
+              }
+            >
+              250
+              <br />
+              ml
+            </div>
+          ))}
       </div>
     </div>
   );
